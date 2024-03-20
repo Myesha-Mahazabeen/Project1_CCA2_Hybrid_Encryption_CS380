c1105e55cfe93842a8e1048bffd5f08821a2db1b
Myesha <maisha.mahjabin27@gmail.com>
2024-03-19 20:35:06 -0400

    updated

--- kem-enc.c
+++ kem-enc.c
@@ -7,30 +7,33 @@
 #include <getopt.h>
 #include <string.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
 #include <openssl/sha.h>
 
 #include "ske.h"
 #include "rsa.h"
 #include "prf.h"
 
-static const char* usage =
-"Usage: %s [OPTIONS]...\n"
-"Encrypt or decrypt data.\n\n"
-"   -i,--in     FILE   read input from FILE.\n"
-"   -o,--out    FILE   write output to FILE.\n"
-"   -k,--key    FILE   the key.\n"
-"   -r,--rand   FILE   use FILE to seed RNG (defaults to /dev/urandom).\n"
-"   -e,--enc           encrypt (this is the default action).\n"
-"   -d,--dec           decrypt.\n"
-"   -g,--gen    FILE   generate new key and write to FILE{,.pub}\n"
-"   -b,--BITS   NBITS  length of new key (NOTE: this corresponds to the\n"
-"                      RSA key; the symmetric key will always be 256 bits).\n"
-"                      Defaults to %lu.\n"
-"   --help             show this message and exit.\n";
+static const char *usage =
+	"Usage: %s [OPTIONS]...\n"
+	"Encrypt or decrypt data.\n\n"
+	"   -i,--in     FILE   read input from FILE.\n"
+	"   -o,--out    FILE   write output to FILE.\n"
+	"   -k,--key    FILE   the key.\n"
+	"   -r,--rand   FILE   use FILE to seed RNG (defaults to /dev/urandom).\n"
+	"   -e,--enc           encrypt (this is the default action).\n"
+	"   -d,--dec           decrypt.\n"
+	"   -g,--gen    FILE   generate new key and write to FILE{,.pub}\n"
+	"   -b,--BITS   NBITS  length of new key (NOTE: this corresponds to the\n"
+	"                      RSA key; the symmetric key will always be 256 bits).\n"
+	"                      Defaults to %lu.\n"
+	"   --help             show this message and exit.\n";
 
 #define FNLEN 255
 
-enum modes {
+enum modes
+{
 	ENC,
 	DEC,
 	GEN
@@ -52,97 +55,276 @@ enum modes {
 
 #define HASHLEN 32 /* for sha256 */
 
-int kem_encrypt(const char* fnOut, const char* fnIn, RSA_KEY* K)
+int kem_encrypt(const char *fnOut, const char *fnIn, RSA_KEY *K)
 {
 	/* TODO: encapsulate random symmetric key (SK) using RSA and SHA256;
 	 * encrypt fnIn with SK; concatenate encapsulation and cihpertext;
 	 * write to fnOut. */
+	size_t len = rsa_numBytesN(K); // length of key
+	unsigned char *x = malloc(len);
+	printf("size %d\n\n", len);
+
+	// generate SK
+	randBytes(x, len);
+	SKE_KEY SK;
+	ske_keyGen(&SK, x, len);
+
+	size_t encapLen = len + HASHLEN; // will be the RSA(X)|H(X)
+	unsigned char *encap = malloc(encapLen);
+
+	// set RSA(X)
+	if (len != rsa_encrypt(encap, x, len, K))
+	{
+		printf("Failed to encrypt RSA\n");
+		return -1;
+	}
+
+	// print unencrypted x
+	/*for(int i = 0; i < len; i++){*/
+	/*printf("%d : %hu\n", i, (unsigned short)(x[i]));*/
+	/*}*/
+
+	// create H(X)
+	unsigned char *h = malloc(HASHLEN);
+	SHA256(x, len, h);
+
+	// add H(x) to back
+	memcpy(encap + len, h, HASHLEN);
+
+	// out file
+	int fdout = open(fnOut, O_CREAT | O_RDWR, S_IRWXU);
+	if (fdout == -1)
+	{
+		printf("Failed to open files");
+		return -1;
+	}
+
+	// write encap to file
+	write(fdout, encap, encapLen);
+	close(fdout);
+
+	// encrypt plaintext with SK
+	// append to encap
+	ske_encrypt_file(fnOut, fnIn, &SK, NULL, encapLen);
+
+	free(x);
+	free(encap);
+	free(h);
 	return 0;
 }
 
 /* NOTE: make sure you check the decapsulation is valid before continuing */
-int kem_decrypt(const char* fnOut, const char* fnIn, RSA_KEY* K)
+int kem_decrypt(const char *fnOut, const char *fnIn, RSA_KEY *K)
 {
 	/* TODO: write this. */
 	/* step 1: recover the symmetric key */
 	/* step 2: check decapsulation */
 	/* step 3: derive key from ephemKey and decrypt data. */
+
+	size_t rsaLen = rsa_numBytesN(K);
+	size_t encapLen = rsaLen + HASHLEN;
+	printf("size %d\n\n", rsaLen);
+
+	FILE *encrypted = fopen(fnIn, "r");
+
+	// get encapsulated -- RSA(X)|H(X)
+	unsigned char *encap = malloc(encapLen);
+	size_t read = fread(encap, 1, encapLen, encrypted);
+	fclose(encrypted);
+
+	// failed to read kem
+	if (read != encapLen)
+	{
+		printf("Failed to read kem");
+		return -1;
+	}
+
+	unsigned char *x = malloc(rsaLen);
+
+	// retrieve x from RSA(X)
+	if (rsaLen != rsa_decrypt(x, encap, rsaLen, K))
+	{
+		printf("Failed to retrieve x");
+		return -1;
+	}
+
+	// print decrypted x
+	// for some reason decrypted x does not match original x
+	/*for(int i = 0; i < rsaLen; i++){*/
+	/*printf("%d : %hu\n", i, (unsigned short) (x[i]));*/
+	/*}*/
+
+	// get H(X)
+	unsigned char *h = malloc(HASHLEN);
+	SHA256(x, rsaLen, h);
+	unsigned char *a = encap + rsaLen;
+
+	// compare h to encapsulated H(x)
+	for (int i = 0; i < HASHLEN; i++)
+	{ // not matching for some reason
+		// printf("%hu\n", (unsigned short)(*h));
+		// printf("%hu\n", (unsigned short)(*a));
+		if (*h != *a)
+		{
+			printf("H(x) did not match\n");
+			return -1;
+		}
+		h++;
+		a++;
+	}
+
+	SKE_KEY SK;
+	ske_keyGen(&SK, x, rsaLen);
+	ske_decrypt_file(fnOut, fnIn, &SK, encapLen);
+	a = NULL;
+	free(h);
+	free(x);
+	free(encap);
 	return 0;
 }
 
-int main(int argc, char *argv[]) {
+int generate(char *fnOut, size_t nBits)
+{
+	RSA_KEY K;
+
+	// create new file with .pub extension
+	char *fPub = malloc(strlen(fnOut) + 5);
+	strcpy(fPub, fnOut);
+	strcat(fPub, ".pub");
+
+	FILE *outPrivate = fopen(fnOut, "w");
+	FILE *outPublic = fopen(fPub, "w");
+
+	rsa_keyGen(nBits, &K);
+	rsa_writePrivate(outPrivate, &K);
+	rsa_writePublic(outPublic, &K);
+
+	fclose(outPrivate);
+	fclose(outPublic);
+	rsa_shredKey(&K);
+	free(fPub);
+	return 0;
+}
+
+int encrypt(char *fnOut, char *fnIn, char *fnKey)
+{
+	FILE *keyFile = fopen(fnKey, "r");
+	printf("Key file: %s\n", fnKey);
+	if (keyFile == NULL)
+	{
+		printf("Key file does not exist\n");
+		return -1;
+	}
+
+	RSA_KEY K;
+	rsa_readPublic(keyFile, &K);
+	kem_encrypt(fnOut, fnIn, &K);
+	rsa_shredKey(&K);
+	fclose(keyFile);
+	return 0;
+}
+
+int decrypt(char *fnOut, char *fnIn, char *fnKey)
+{
+	FILE *privateKey = fopen(fnKey, "r");
+	printf("Key file: %s\n", fnKey);
+	if (privateKey == NULL)
+	{
+		printf("Key file does not exist\n");
+		return -1;
+	}
+
+	RSA_KEY K;
+	rsa_readPrivate(privateKey, &K);
+	fclose(privateKey);
+	kem_decrypt(fnOut, fnIn, &K);
+	rsa_shredKey(&K);
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
 	/* define long options */
 	static struct option long_opts[] = {
-		{"in",      required_argument, 0, 'i'},
-		{"out",     required_argument, 0, 'o'},
-		{"key",     required_argument, 0, 'k'},
-		{"rand",    required_argument, 0, 'r'},
-		{"gen",     required_argument, 0, 'g'},
-		{"bits",    required_argument, 0, 'b'},
-		{"enc",     no_argument,       0, 'e'},
-		{"dec",     no_argument,       0, 'd'},
-		{"help",    no_argument,       0, 'h'},
-		{0,0,0,0}
-	};
+		{"in", required_argument, 0, 'i'},
+		{"out", required_argument, 0, 'o'},
+		{"key", required_argument, 0, 'k'},
+		{"rand", required_argument, 0, 'r'},
+		{"gen", required_argument, 0, 'g'},
+		{"bits", required_argument, 0, 'b'},
+		{"enc", no_argument, 0, 'e'},
+		{"dec", no_argument, 0, 'd'},
+		{"help", no_argument, 0, 'h'},
+		{0, 0, 0, 0}};
 	/* process options: */
 	char c;
 	int opt_index = 0;
-	char fnRnd[FNLEN+1] = "/dev/urandom";
+	char fnRnd[FNLEN + 1] = "/dev/urandom";
 	fnRnd[FNLEN] = 0;
-	char fnIn[FNLEN+1];
-	char fnOut[FNLEN+1];
-	char fnKey[FNLEN+1];
-	memset(fnIn,0,FNLEN+1);
-	memset(fnOut,0,FNLEN+1);
-	memset(fnKey,0,FNLEN+1);
+	char fnIn[FNLEN + 1];
+	char fnOut[FNLEN + 1];
+	char fnKey[FNLEN + 1];
+	memset(fnIn, 0, FNLEN + 1);
+	memset(fnOut, 0, FNLEN + 1);
+	memset(fnKey, 0, FNLEN + 1);
 	int mode = ENC;
 	// size_t nBits = 2048;
 	size_t nBits = 1024;
-	while ((c = getopt_long(argc, argv, "edhi:o:k:r:g:b:", long_opts, &opt_index)) != -1) {
-		switch (c) {
-			case 'h':
-				printf(usage,argv[0],nBits);
-				return 0;
-			case 'i':
-				strncpy(fnIn,optarg,FNLEN);
-				break;
-			case 'o':
-				strncpy(fnOut,optarg,FNLEN);
-				break;
-			case 'k':
-				strncpy(fnKey,optarg,FNLEN);
-				break;
-			case 'r':
-				strncpy(fnRnd,optarg,FNLEN);
-				break;
-			case 'e':
-				mode = ENC;
-				break;
-			case 'd':
-				mode = DEC;
-				break;
-			case 'g':
-				mode = GEN;
-				strncpy(fnOut,optarg,FNLEN);
-				break;
-			case 'b':
-				nBits = atol(optarg);
-				break;
-			case '?':
-				printf(usage,argv[0],nBits);
-				return 1;
+	while ((c = getopt_long(argc, argv, "edhi:o:k:r:g:b:", long_opts, &opt_index)) != -1)
+	{
+		switch (c)
+		{
+		case 'h':
+			printf(usage, argv[0], nBits);
+			return 0;
+		case 'i':
+			strncpy(fnIn, optarg, FNLEN);
+			break;
+		case 'o':
+			strncpy(fnOut, optarg, FNLEN);
+			break;
+		case 'k':
+			strncpy(fnKey, optarg, FNLEN);
+			break;
+		case 'r':
+			strncpy(fnRnd, optarg, FNLEN);
+			break;
+		case 'e':
+			mode = ENC;
+			break;
+		case 'd':
+			mode = DEC;
+			break;
+		case 'g':
+			mode = GEN;
+			strncpy(fnOut, optarg, FNLEN);
+			break;
+		case 'b':
+			nBits = atol(optarg);
+			break;
+		case '?':
+			printf(usage, argv[0], nBits);
+			return 1;
 		}
 	}
 
 	/* TODO: finish this off.  Be sure to erase sensitive data
 	 * like private keys when you're done with them (see the
 	 * rsa_shredKey function). */
-	switch (mode) {
-		case ENC:
-		case DEC:
-		case GEN:
-		default:
-			return 1;
+
+	switch (mode)
+	{
+	case ENC:
+		encrypt(fnOut, fnIn, fnKey);
+		break;
+	case DEC:
+		decrypt(fnOut, fnIn, fnKey);
+		break;
+	case GEN:
+		generate(fnOut, nBits);
+		break;
+	default:
+		return 1;
 	}
 
 	return 0;
--- ske.c
+++ ske.c
@@ -2,6 +2,7 @@
 #include "prf.h"
 #include <openssl/sha.h>
 #include <openssl/evp.h>
+#include <openssl/err.h>
 #include <openssl/hmac.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,8 +12,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/mman.h>
-#include <openssl/err.h>
-
 #ifdef LINUX
 #define MMAP_SEQ MAP_PRIVATE | MAP_POPULATE
 #else
@@ -22,9 +21,9 @@
 /* NOTE: since we use counter mode, we don't need padding, as the
  * ciphertext length will be the same as that of the plaintext.
  * Here's the message format we'll use for the ciphertext:
- * +------------+--------------------+----------------------------------+
- * | 16 byte IV | C = AES(plaintext) | HMAC(IV|C) (32 bytes for SHA256) |
- * +------------+--------------------+----------------------------------+
+ * +------------+--------------------+-------------------------------+
+ * | 16 byte IV | C = AES(plaintext) | HMAC(C) (32 bytes for SHA256) |
+ * +------------+--------------------+-------------------------------+
  * */
 
 /* we'll use hmac with sha256, which produces 32 byte output */
@@ -38,7 +37,26 @@ int ske_keyGen(SKE_KEY *K, unsigned char *entropy, size_t entLen)
 	/* TODO: write this.  If entropy is given, apply a KDF to it to get
 	 * the keys (something like HMAC-SHA512 with KDF_KEY will work).
 	 * If entropy is null, just get a random key (you can use the PRF). */
-	
+
+	if (entropy == NULL)
+	{
+		randBytes(K->hmacKey, 32);
+		randBytes(K->aesKey, 32);
+	}
+	else
+	{
+		unsigned char *outBuf_64;
+		outBuf_64 = malloc(64);
+		HMAC(EVP_sha512(), KDF_KEY, 32, entropy, entLen, outBuf_64, NULL);
+
+		for (int i = 0; i < 32; i++)
+		{
+			K->hmacKey[i] = outBuf_64[i];
+			K->aesKey[i] = outBuf_64[i + 32];
+		}
+
+		free(outBuf_64);
+	}
 
 	return 0;
 }
@@ -52,20 +70,104 @@ size_t ske_encrypt(unsigned char *outBuf, unsigned char *inBuf, size_t len,
 	/* TODO: finish writing this.  Look at ctr_example() in aes-example.c
 	 * for a hint.  Also, be sure to setup a random IV if none was given.
 	 * You can assume outBuf has enough space for the result. */
-	
 
-	return 0; /* TODO: should return number of bytes written, which
-				 hopefully matches ske_getOutputLen(...). */
+	if (IV == NULL)
+	{
+		IV = malloc(16);
+		randBytes(IV, 16);
+	}
+	memcpy(outBuf, IV, 16);
+
+	EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+	if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_ctr(), 0, K->aesKey, IV))
+	{
+		ERR_print_errors_fp(stderr);
+	}
+	int nWritten;
+	if (1 != EVP_EncryptUpdate(ctx, outBuf + 16, &nWritten, inBuf, len))
+	{
+		ERR_print_errors_fp(stderr);
+	}
+	EVP_CIPHER_CTX_free(ctx);
+
+	int totalLen = nWritten + 16 + HM_LEN;
+	unsigned char newBuf[nWritten];
+	memcpy(newBuf, &outBuf[16], nWritten);
+
+	unsigned char *HMAC_Buf = malloc(HM_LEN);
+	HMAC(EVP_sha256(), K->hmacKey, HM_LEN, outBuf, nWritten + 16, HMAC_Buf, NULL);
+	memcpy(&outBuf[nWritten + 16], HMAC_Buf, HM_LEN);
+
+	return totalLen; /* TODO: should return number of bytes written, which
+								 hopefully matches ske_getOutputLen(...). */
 }
 size_t ske_encrypt_file(const char *fnout, const char *fnin,
 						SKE_KEY *K, unsigned char *IV, size_t offset_out)
 {
 	/* TODO: write this.  Hint: mmap. */
-	
 
+	int fdin = open(fnin, O_RDONLY);
+	if (fdin == -1)
+	{
+		printf("Failed to open %s\n", fnin);
+		return -1;
+	}
+	int fdout = open(fnout, O_CREAT | O_RDWR, S_IRWXU);
+	if (fdout == -1)
+	{
+		printf("Failed to open %s\n", fdout);
+		return -1;
+	}
+
+	struct stat buf;
+	if (fstat(fdin, &buf) == -1 || buf.st_size == 0)
+	{
+		return -1;
+	}
+
+	char *pa;
+	pa = mmap(NULL, buf.st_size, PROT_READ, MAP_PRIVATE, fdin, 0); // mmap() establish a mapping between a process address space and a file
+	if (pa == MAP_FAILED)
+	{
+		return -1;
+	}
+
+	size_t fdinLen = strlen(pa) + 1;
+	size_t ciphertextLen = ske_getOutputLen(fdinLen);
+
+	unsigned char *ciphertext = malloc(ciphertextLen + 1);
+
+	char freeIV = 0;
+	if (IV == NULL)
+	{
+		IV = malloc(16);
+		randBytes(IV, 16);
+		freeIV = 1;
+	}
+	ssize_t encryptLen = ske_encrypt(ciphertext, (unsigned char *)pa, fdinLen, K, IV);
+
+	if (encryptLen == -1)
+	{
+		printf("Failed to encrypt\n");
+	}
+
+	lseek(fdout, offset_out, SEEK_SET);
+	ssize_t written = write(fdout, ciphertext, encryptLen);
+	if (written == -1)
+	{
+		printf("Failed to write to file\n");
+	}
+
+	munmap(pa, buf.st_size);
+	free(ciphertext);
+	if (freeIV > 0)
+	{
+		free(IV);
+	}
+	close(fdin);
+	close(fdout);
 	return 0;
 }
-
 size_t ske_decrypt(unsigned char *outBuf, unsigned char *inBuf, size_t len,
 				   SKE_KEY *K)
 {
@@ -73,15 +175,82 @@ size_t ske_decrypt(unsigned char *outBuf, unsigned char *inBuf, size_t len,
 	 * Oh, and also, return -1 if the ciphertext is found invalid.
 	 * Otherwise, return the number of bytes written.  See aes-example.c
 	 * for how to do basic decryption. */
-	
-	return 0;
-}
 
+	unsigned char hmac[HM_LEN];
+	HMAC(EVP_sha256(), K->hmacKey, HM_LEN, inBuf, len - HM_LEN, hmac, NULL);
+
+	for (int i = 0; i < HM_LEN; i++)
+	{
+		if (hmac[i] != inBuf[len - HM_LEN + i])
+		{
+			return -1;
+		}
+	}
+
+	unsigned char IV[16];
+	memcpy(IV, inBuf, 16);
+
+	int adjustLen = len - HM_LEN - 16;
+	unsigned char ciphertext[adjustLen];
+	for (int i = 0; i < adjustLen; i++)
+	{
+		ciphertext[i] = inBuf[i + 16];
+	}
+
+	EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+	if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_ctr(), 0, K->aesKey, IV))
+	{
+		ERR_print_errors_fp(stderr);
+	}
+
+	size_t ciphertextLen = adjustLen;
+
+	int nWritten = 0;
+	if (1 != EVP_DecryptUpdate(ctx, outBuf, &nWritten, ciphertext, ciphertextLen))
+	{
+		ERR_print_errors_fp(stderr);
+	}
+
+	return nWritten;
+}
 size_t ske_decrypt_file(const char *fnout, const char *fnin,
 						SKE_KEY *K, size_t offset_in)
 {
 	/* TODO: write this. */
-	
+
+	int fdin = open(fnin, O_RDONLY);
+	int fdout = open(fnout, O_CREAT | O_RDWR, S_IRWXU);
+	if (fdin == -1 || fdout == -1)
+	{
+		return -1;
+	}
+
+	struct stat buf;
+	if (fstat(fdin, &buf) == -1 || !buf.st_size)
+	{
+		return -1;
+	}
+
+	unsigned char *pa;
+	pa = mmap(NULL, buf.st_size, PROT_READ, MAP_PRIVATE, fdin, offset_in);
+	if (pa == MAP_FAILED)
+	{
+		return -1;
+	}
+
+	char *plaintext = malloc(buf.st_size - 16 - HM_LEN - offset_in);
+	ske_decrypt((unsigned char *)plaintext, pa, buf.st_size - offset_in, K);
+	// write(fdout, plaintext, buf.st_size-16-HM_LEN);
+	FILE *pFile = fopen(fnout, "w");
+	if (pFile == NULL)
+	{
+		return -1;
+	}
+	else
+	{
+		fputs(plaintext, pFile);
+		fclose(pFile);
+	}
 
 	return 0;
 }
